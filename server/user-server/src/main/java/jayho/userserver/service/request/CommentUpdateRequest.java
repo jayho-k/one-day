c7d3a1d391e990f65cedb40dcb52c8c3964e0065
jayho-k <sox3579@gmail.com>
2025-06-26 20:05:32 +0900

    feat : CommentController 생성

---
+++ server/user-server/src/main/java/jayho/userserver/service/request/CommentUpdateRequest.java
@@ -0,0 +1,18 @@
+package jayho.userserver.service.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CommentUpdateRequest {
+
+    @NotNull
+    private Long articleId;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private Long writerId;
+
+}
