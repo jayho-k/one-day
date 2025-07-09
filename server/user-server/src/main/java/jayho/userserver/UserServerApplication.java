a2c6c185523406470892170214b4100991227c17
jayho-k <sox3579@gmail.com>
2025-07-09 22:32:29 +0900

    chore : 멀티 모듈 사용을 위한 @SpringBootApplication scanBasePackages 추가 및 gralde setting

--- server/user-server/src/main/java/jayho/userserver/UserServerApplication.java
+++ server/user-server/src/main/java/jayho/userserver/UserServerApplication.java
@@ -3,9 +3,8 @@ package jayho.userserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"jayho.userserver", "jayho.useractiverdb"})
 public class UserServerApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(UserServerApplication.class, args);
