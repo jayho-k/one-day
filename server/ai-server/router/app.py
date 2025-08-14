281d59acc0492dff11da29d321ee226c93189cb2
jayho-k <sox3579@gmail.com>
2025-08-14 22:47:08 +0900

    chore : ai-server 환경세팅

---
+++ server/ai-server/router/app.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"안녕": "FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
